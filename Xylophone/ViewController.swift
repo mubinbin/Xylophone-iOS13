ea34ff1c9be560556544ac917561cd84422e5db4
mubinbin <dinobunmu@gmail.com>
2024-10-05 00:13:12 -0500

    added play sound

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,15 +6,26 @@
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func pressButton(_ sender: UIButton) {
+        playSound(sender.currentTitle!)
+    }
+    
+    func playSound(_ tone: String) {
+        let url = Bundle.main.url(forResource: tone, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
     
 
 }
